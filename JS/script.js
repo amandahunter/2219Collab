c8d85e1c6412f6fb0a6e9f2e09d634eb94bae2b8
Becca Carmichael <becca.carmichael@flemingcollege.ca>
2022-04-07 00:33:27 -0400

    BC

--- JS/script.js
+++ JS/script.js
@@ -1,27 +1,25 @@
 // Navbar - KS & BC - April 5 2022 
 function myFunction() {
-    var x = document.getElementById("myTopnav");
+    var x = document.getElementById("mytopnav");
     if (x.className === "topnav") {
-      x.className += " responsive";
+      x.className += "responsive";
     } else {
       x.className = "topnav";
     }
   }
-//Get the button
-var topbutton = document.getElementById("topbutton");
+//Scroll Back to Top Button - BC 
+var mybutton = document.getElementById("topBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-    topbutton.style.display = "block";
+    mybutton.style.display = "block";
   } else {
-    topbutton.style.display = "none";
+    mybutton.style.display = "none";
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
