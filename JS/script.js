d5f8ba488b61c839ad3f0591c599548a831e6ff6
K Soodhoo <kathleen.soodhoo@flemingcollege.ca>
2022-04-14 11:06:28 -0400

    forgot jpg

--- JS/script.js
+++ JS/script.js
@@ -29,7 +29,7 @@ function topFunction() {
 // ks april 14 
 
 var slider_img = document.querySelector('.slider-img');
-var images = [ 'S1_1', 'S1_2'];
+var images = [ 'S1_1.jpg', 'S1_2.jpg'];
 var i = 0;
 
 function prev(){
